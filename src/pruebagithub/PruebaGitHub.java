2997616fffe223cc0723c82d1f2400adbf44d50a
Harold <Harold@DESKTOP-AFGP76O>
2021-07-25 08:50:28 -0500

    Se carga mensaje de prueba

--- src/pruebagithub/PruebaGitHub.java
+++ src/pruebagithub/PruebaGitHub.java
@@ -16,7 +16,7 @@ public class PruebaGitHub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        System.err.println("Primer mensaje");
     }
     
 }
